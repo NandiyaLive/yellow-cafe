f8857e9ae3a90dde769dfbcebda98e93e1edbb1e
Neranjana Prasad <30690726+NandiyaLive@users.noreply.github.com>
2022-12-06 11:36:40 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
       <hr />
       <div className="flex items-start justify-between sm:block sm:space-y-2 sm:text-center mt-4">
         <p>
-          Copyright &copy; {new Date().getFullYear()} <a href="https://dynamicbiz.org.lk" target="_blank" rel="noopener noreferrer" className="font-semibold underline underline-offset-4">Dynamic Biz</a>. All Rights Reserved.
+          {/* Copyright &copy; {new Date().getFullYear()} <a href="https://dynamicbiz.org.lk" target="_blank" rel="noopener noreferrer" className="font-semibold underline underline-offset-4">Dynamic Biz</a>. All Rights Reserved. */}
         </p>
 
         <div className="flex items-center gap-4 sm:justify-center">
